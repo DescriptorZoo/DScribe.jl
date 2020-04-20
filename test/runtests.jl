59a25f36bf52bedbc0a1dbe08db6ad35b2872a08
berkonat <b.onat@warwick.ac.uk>
2020-04-20 21:57:42 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using JuLIP, JuLIP.Testing
 using DScribe: dscribe_acsf, dscribe_soap
 
 @testset "DScribe.jl" begin
-    include("acsf_test.jl")
+    #include("acsf_test.jl")
     include("soap_test.jl")
 end
 
